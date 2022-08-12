a9632d7c851294f6174163ac956286bdbe29a303
Nquxii <famousneve@gmail.com>
2022-08-12 13:20:37 -0400

    use dotenv instead of json

--- data/config.py
+++ data/config.py
@@ -1,18 +1,21 @@
 import json
 import os
+import sys
+from dotenv import load_dotenv
 
 # define file directories
 script_dir = os.path.dirname(__file__)
 config_path = 'config.json'
 config_file = os.path.join(script_dir, config_path)
 
-# open config file of 
-with open(config_file) as f:
-    # load configuration file as config
-    config = json.load(f)
+# load dotenv
+load_dotenv()
 
-    # bot properties
-    discordtoken = config['SETTINGS']['discordtoken'].strip()
-    prefix = config['SETTINGS']['default_prefix'].strip()
-    server = int(config['SETTINGS']['guildID'].strip())
-    owner = config['SETTINGS']['owner'].strip()
+# bot properties
+discordtoken = str(os.getenv('TOKEN'))
+owner = str(os.getenv('owner'))
+prefix = str(os.getenv('default_prefix'))
+server = str(os.getenv('guildID'))
+
+if not discordtoken:
+    sys.exit(".env file with token not found")
